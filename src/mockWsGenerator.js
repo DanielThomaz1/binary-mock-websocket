3008542603762ce81339778c24bb6769f546bc20
Amin Marashi <marashi.s.amin@gmail.com>
2016-09-08 10:41:03 +0800

    Used live api spy function to determine the req_ids

--- src/mockWsGenerator.js
+++ src/mockWsGenerator.js
@@ -2,24 +2,23 @@ import { LiveApi } from 'binary-live-api';
 import { observer } from 'binary-common-utils/lib/observer';
 import WebSocket from 'ws';
 
-class SpyWebSocket extends WebSocket {
-  constructor(...args) {
-    super(...args);
-    this.respDatabase = {};
-  }
-}
-
 export default class MockWsGenerator {
   constructor(calls) {
     this.calls = calls;
     this.sharedContext = {};
+    this.respDatabase = {};
+    this.reqIdToReq = {};
     this.api = new LiveApi({
-      websocket: SpyWebSocket,
+      websocket: WebSocket,
+      sendSpy: (rawData) => {
+        const data = JSON.parse(rawData);
+        this.reqIdToReq[data.req_id] = rawData;
+      },
     });
   }
   async generate() {
-    await this.iterateCalls(this.calls, this.api.socket.respDatabase);
-    return 'module.exports = ' + JSON.stringify(this.api.socket.respDatabase).replace("'", "\\'") + ';';
+    await this.iterateCalls(this.calls, this.respDatabase);
+    return 'module.exports = ' + JSON.stringify(this.respDatabase).replace("'", "\\'") + ';';
   }
   async iterateCalls(calls, respDatabase) {
     for (let callName of Object.keys(calls)) {
@@ -33,36 +32,36 @@ export default class MockWsGenerator {
         for (let callDefName of Object.keys(callDefs)) {
           let callDef = callDefs[callDefName];
           callDef.func(this.api, this.sharedContext);
-					let logger = console;
-					logger.log(callDefName);
+          let logger = console;
+          logger.log(callDefName);
           if (callResTypeName === 'subscriptions') {
             if (callName === 'history') {
-              this.api.events.on((callResTypeName === 'errors')? 'error': 'history', (ah) => observer.emit('data.history', ah));
+              this.api.events.on((callResTypeName === 'errors') ? 'error' : 'history', (ah) => observer.emit('data.history', ah));
               let data = await new Promise((r) => observer.register('data.history', r, true));
               this.handleDataSharing(data);
               respDatabase[callName][callResTypeName][this.getKeyFromReq(data)] = {
                 data: [data],
               };
-              this.api.events.on((callResTypeName === 'errors')? 'error': 'tick', (at) => observer.emit('data.tick', at));
+              this.api.events.on((callResTypeName === 'errors') ? 'error' : 'tick', (at) => observer.emit('data.tick', at));
               await this.observeSubscriptions('data.tick', respDatabase, callDef);
             } else {
-              this.api.events.on((callResTypeName === 'errors')? 'error': callName, (an) => observer.emit('data.' + callName, an));
+              this.api.events.on((callResTypeName === 'errors') ? 'error' : callName, (an) => observer.emit('data.' + callName, an));
               await this.observeSubscriptions('data.' + callName, respDatabase, callDef);
             }
           } else {
-            this.api.events.on((callResTypeName === 'errors')? 'error': callName, (ano) => observer.emit('data.' + callName, ano));
+            this.api.events.on((callResTypeName === 'errors') ? 'error' : callName, (ano) => observer.emit('data.' + callName, ano));
             let data = await new Promise((r) => observer.register('data.' + callName, r, true));
             this.handleDataSharing(data);
             let key = this.getKeyFromReq(data);
             let reqDef = respDatabase[callName][callResTypeName][key] = {
               data,
             };
-						await this.iterateNext(callDef, reqDef);
+            await this.iterateNext(callDef, reqDef);
           }
         }
       }
     }
-		return;
+    return;
   }
   async observeSubscriptions(event, respDatabase, callDef) {
     let reqDef = await new Promise((r) => {
@@ -88,12 +87,12 @@ export default class MockWsGenerator {
     });
     await this.iterateNext(callDef, reqDef);
   }
-	async iterateNext(callDef, reqDef) {
-		if (callDef.next) {
-			reqDef.next = {};
-			await this.iterateCalls(callDef.next, reqDef.next);
-		}
-	}
+  async iterateNext(callDef, reqDef) {
+    if (callDef.next) {
+      reqDef.next = {};
+      await this.iterateCalls(callDef.next, reqDef.next);
+    }
+  }
   handleSubscriptionLimits(data, defData, callDef) {
     defData.push(data);
     if (defData.length === callDef.maxResponse || (callDef.stopCondition && callDef.stopCondition(data))) {
@@ -119,6 +118,8 @@ export default class MockWsGenerator {
     }
   }
   getKeyFromReq(data) {
-    return JSON.stringify(data.echo_req);
+    if (data.req_id in this.reqIdToReq) {
+      return this.reqIdToReq[data.req_id];
+    }
   }
 }
